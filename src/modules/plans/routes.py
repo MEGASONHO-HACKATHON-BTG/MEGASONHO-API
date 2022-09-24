af77d5c60757e8379d441e860433223cdb1bb0d3
brunohardcode <bruno.santos@hardcode.express>
2022-09-24 20:05:08 -0300

    new feature

--- src/modules/plans/routes.py
+++ src/modules/plans/routes.py
@@ -8,6 +8,7 @@ from src.modules.plans.models import CreatePlanModel, PlanPayloadModel
 
 from src.modules.plans.service.create_plan_service import CreatePlanService
 from src.modules.plans.service.list_plan_service import ListPlanService
+from src.modules.plans.service.purchase_plan_service import PurchasePlanService
 
 router = APIRouter(
     prefix='/plans',
@@ -23,8 +24,17 @@ def create_plan(
     return service.execute(model)
 
 @router.get('/list/', response_model=List[PlanPayloadModel])
-def create_plan(
+def list_plan(
     db: Session = Depends(get_database)
 ):
     service = ListPlanService(db)
     return service.execute()
+
+@router.post('/purchase/', response_model=bool)
+def purchase_plan(
+    document: str = Query(),
+    uuid: str = Query(),
+    db: Session = Depends(get_database)
+):
+    service = PurchasePlanService(db)
+    return service.execute(uuid, document)
